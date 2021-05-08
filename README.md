3b20b04f92fc1af0c8b7b1c0d31732a8951a5d2e
Arun R <arun@ftltech.io>
2021-05-08 18:11:01 +0530

    Improv: Working repro of issue

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# vue-cli-babel-corejs-exclude-polyfill
-Reproduction for vue-cli bug with cli-plugin babel being unable to exclude core-js polyfills
+# demo
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Run your unit tests
+```
+yarn test:unit
+```
+
+### Run your end-to-end tests
+```
+yarn test:e2e
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>Polyfill exclusion principle</h1>
+    <h2>Status: {{ status }}</h2>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+  data() {
+    return {
+      status: "Loading",
+    };
+  },
+  mounted() {
+    const url = new URL("http://www.google.com/?data=unknown");
+    this.status = `Loaded: ${url.toString()}`;
+  },
+});
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
